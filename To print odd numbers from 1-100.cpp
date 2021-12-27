2d9de86304cb8d3f8106d7236bc73ccc459075cd
Saksham Verma <dripclade@gmail.com>
2021-12-27 22:39:37 +0530

    Create To print odd numbers from 1-100.cpp

---
+++ To print odd numbers from 1-100.cpp
@@ -0,0 +1,11 @@
+// Program to find out the odd numbers between 1 & 100
+#include <stdio.h>
+int main(){
+	printf("The Odd numbers from 1-100 are as follows:");
+{
+	for(int i = 1;i<=100;i+=2)
+	printf("\n%d", i);
+}
+return 0;
+         
+}
