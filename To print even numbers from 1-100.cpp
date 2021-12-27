a0077c5e7cf3b83a7e6b1d612490b6c542d9ea5b
Saksham Verma <dripclade@gmail.com>
2021-12-27 22:39:44 +0530

    Create To print even numbers from 1-100.cpp

---
+++ To print even numbers from 1-100.cpp
@@ -0,0 +1,11 @@
+// Program to find out the even numbers between 1 & 100
+#include <stdio.h>
+int main(){
+	printf("The Even numbers from 1-100 are as follows:");
+{
+	for(int i = 2;i<=100;i+=2)
+	printf("\n%d", i);
+}
+return 0;
+         
+}
