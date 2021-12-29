0c7696ed1b38511a2f15d34d627a0f72fefffc11
Saksham Verma <dripclade@gmail.com>
2021-12-29 22:59:37 +0530

    Create Lower Triangle.cpp

---
+++ Lower Triangle.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+	int n = 10;
+	for (int i = 1; i<=n; i++){
+		for(int j = 1; j<=n; j++){
+			if (i+j>n)
+			printf("* ");
+			else{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
