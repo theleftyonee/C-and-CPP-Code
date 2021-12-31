53dd65167193d265d7dc8b00fb6a8005adb76c7f
Saksham Verma <dripclade@gmail.com>
2021-12-31 13:25:24 +0530

    Create Equilateral triangle.cpp

---
+++ Equilateral triangle.cpp
@@ -0,0 +1,17 @@
+// Program to get an equilateral triangle
+#include <stdio.h>
+int main(){
+	int n = 10;
+	printf("Enter the number of rows: ");
+	scanf("%d", &n);
+	for(int i = 1;i <= n; i++){
+		for (int j = i; j < n;j++){
+			printf(" ");
+		}
+		for (int j = 1;j<=(2*i-1); j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
