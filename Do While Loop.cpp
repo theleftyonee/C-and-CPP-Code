ebda17ab9a6f29bf5e8cece549d9b1a717a0f6cc
Saksham Verma <96550051+theleftyonee@users.noreply.github.com>
2022-07-23 14:33:37 +0530

    Update Do While Loop.cpp

--- Do While Loop.cpp
+++ Do While Loop.cpp
@@ -1,9 +1,14 @@
+#include <iostream>
 #include <stdio.h>
-int main (){
-	int i = 5;
-	do{
-		printf("%d\n", i);
+#include <conio.h>
+int main(){
+	int n;
+	printf("Enter the numbers till when you want to get printed: ");
+	scanf("%d", &n);
+	int i=0;
+	while(i<=n){
+		printf("%d \n",i);
 		i++;
-	}while (i<=5);
+	}
 	return 0;
 }
